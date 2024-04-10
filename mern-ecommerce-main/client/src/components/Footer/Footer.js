97fdceaa94c1d55d631f34c9bb7ec0f760e146a6
Pranav Sahu <pranavsahu28@gmail.com>
2024-04-10 10:58:28 +0530

    small changes

--- mern-ecommerce-main/client/src/components/Footer/Footer.js
+++ mern-ecommerce-main/client/src/components/Footer/Footer.js
@@ -38,13 +38,13 @@ const Footer = () => {
                         </div>
                         <ul className="footer__contacts">
                             <li className="footer__contact">
-                                <LocalPhoneIcon /> <span>+123 4567 890</span>
+                                <LocalPhoneIcon /> <span>+91-8319905120</span>
                             </li>
                             <li className="footer__contact">
                                 <EmailIcon /> <span>shop@shema.com</span>
                             </li>
                             <li className="footer__contact">
-                                <LocationOnIcon /> <span>Addis Ababa, Ethiopia</span>
+                                <LocationOnIcon /> <span>Chhattisgarh, India</span>
                             </li>
                         </ul>
                     </div>
@@ -73,7 +73,7 @@ const Footer = () => {
                 </div>
                 <div className="fotter__copyright__container">
                     <ul className='nav'>
-                        <li className="footer__copyright">©2022 Shema Ltd. |</li>
+                        <li className="footer__copyright">©2024 Shema Ltd. |</li>
                         <li className="footer__terms__condition"> | Terms & Condition |</li>
                         <li className="footer__privacy__policy">| Privacy Policy</li>
                     </ul>
